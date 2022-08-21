93a80df0ce16e193563f5cd01b42dada87eadcd0
AnandRajbhar <108142632+AnandRajbhar@users.noreply.github.com>
2022-08-21 09:59:49 +0530

    my news application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import "./App.css";
+import NavBar from "./component/NavBar";
+import News from "./component/News";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="News7*24">
+      {/* <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route
+            path="/"
+            element={<News pageSize={6} country="in" category="General" />}
+          />
+        </Routes>
+      </BrowserRouter> */}
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route
+            path="/"
+            element={<News pageSize={6} country="in" category="General" />}
+          />
+          <Route
+            path="/Home"
+            element={<News pageSize={6} country="in" category="General" />}
+          />
+          <Route
+            path="/Business"
+            element={<News pageSize={6} country="in" category="Business" />}
+          />
+          <Route
+            path="/Entertainment"
+            element={
+              <News pageSize={6} country="in" category="Entertainment" />
+            }
+          />
+          <Route
+            path="/General"
+            element={<News pageSize={6} country="in" category="General" />}
+          />
+          <Route
+            path="/Health"
+            element={<News pageSize={6} country="in" category="Health" />}
+          />
+          <Route
+            path="/Science"
+            element={<News pageSize={6} country="in" category="Science" />}
+          />
+          <Route
+            path="/Sports"
+            element={<News pageSize={6} country="in" category="Sports" />}
+          />
+          <Route
+            path="/Technology"
+            element={<News pageSize={6} country="in" category="Technology" />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
